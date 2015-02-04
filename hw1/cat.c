90b09064c67d32b498b587f533e73c687754385d
Tuvia Lerea <tuvia.lerea@gmail.com>
2015-02-03 21:04:59 -0500

    still working on c

--- hw1/cat.c
+++ hw1/cat.c
@@ -3,9 +3,9 @@
 
 int main(void) {
 	int buf_size = 4096;
-	int count = 0;
-	char buf[buf_size];
-	while ((count = read(stdin, buf, buf_size)) > 0) {
-		write(stdout, buf, count);
+	char *buf;
+	size_t count;
+	while ((buf = fgetln(stdin, &count)) != NULL) {
+		fwrite(buf, 1, count, stdout);
 	}
 }
