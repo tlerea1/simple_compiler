c33b246b816d16bc2c78e3c5738f958bb87a4b19
Tuvia Lerea <tuvia.lerea@gmail.com>
2015-02-04 02:30:03 +0000

    still working on c compiling, have python working need to test binary

---
+++ hw1/cat.py
@@ -0,0 +1,8 @@
+import sys
+
+
+while 1:
+	line = sys.stdin.readline()
+	if not line:
+		break
+	sys.stdout.write(line)
---
+++ hw1/cat.s
@@ -0,0 +1,9 @@
+#cat in MIPS assembly
+
+buf_size = 4096
+read string = 8
+
+.data
+buf: .space 4096
+.text
+
