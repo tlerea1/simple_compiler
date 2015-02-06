4d8b38048af52023a8ee71365a3d86ed3f63dbaa
Tuvia Lerea <tuvia.lerea@gmail.com>
2015-02-05 20:35:06 -0500

    fixed c program wrote Makefile and README

--- hw1/cat.c
+++ hw1/cat.c
@@ -1,9 +1,10 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <unistd.h>
 
 int main(void) {
 	int buf_size = 4096;
-	char *buf;
+	char buf[buf_size];
 	size_t count;
 	while ((count = read(0, buf, buf_size)) > 0) {
 		write(1, buf, count);
